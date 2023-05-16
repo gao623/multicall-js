22e63db5c5c289f0eeadc11def4b76c3b5581205
sharon-garcia <linaly.gs@gmail.com>
2023-05-16 15:45:36 +0800

    update test

--- test/testV2.js
+++ test/testV2.js
@@ -2,13 +2,16 @@ const Web3 = require('web3');
 const TronWeb = require('tronweb');
 const { MultiCall } = require("../");
 
-async function multiCall(client, config, multiCallType) {
+async function multiCall(client, config, multiCallType,
+  aggregateFuncName = MultiCall.aggregateFuncNameDict.aggregate,
+  includesAllOutputsWithStatus = MultiCall.includesAllOutputsWithStatus
+) {
   console.log("multiCallType", multiCallType)
   const {multiCallAddr, targetInfo} = config;
-  const aggregateResult = await MultiCall.aggregate(client, multiCallAddr, targetInfo, {multiCallType: multiCallType});
+  const aggregateResult = await MultiCall.aggregate(client, multiCallAddr, targetInfo, {multiCallType, aggregateFuncName, includesAllOutputsWithStatus});
   console.log("aggregateResult:", aggregateResult.blockNumber, aggregateResult.result);
-  const multiRequestResult = await MultiCall[multiCallType](client, multiCallAddr, targetInfo);
-  const result = await MultiCall.decodeEthMultiCallV2(targetInfo, multiRequestResult);
+  const multiRequestResult = await MultiCall[multiCallType](client, multiCallAddr, targetInfo, {aggregateFuncName});
+  const result = await MultiCall.decodeEthMultiCallV2(targetInfo, multiRequestResult, {includesAllOutputsWithStatus});
   return result;
 }
 
@@ -71,92 +74,104 @@ function getTrxClient(nodeUrl, apiKey) {
   return tronWeb;
 }
 
-async function test(config, chainTypeDict) {
+async function test(config, chainTypeDict,
+  aggregateFuncName = MultiCall.aggregateFuncNameDict.aggregate,
+  includesAllOutputsWithStatus = MultiCall.includesAllOutputsWithStatus
+) {
   for (let chainType in config) {
     if (chainType === chainTypeDict.TRX) {
       const {nodeUrl, apiKey, multiCallAddr} = config[chainType];
       const client = getTrxClient(nodeUrl, apiKey);
       // console.log(chainType, "client keys", Object.keys(client), client)
       console.log(chainType, multiCallAddr, "is", client.customApi.toEthereumAddress("TDGSR64oU4QDpViKfdwawSiqwyqpUB6JUD"));
-      // const result = await multiCall(client, config[chainType], "MultiCall");
-      const result = await multiCall(client, config[chainType], "trxMultiCall");
-      console.log(chainType, "final result:", result.blockNumber, result.result);
+      const result = await multiCall(client, config[chainType], "trxMultiCall", aggregateFuncName, includesAllOutputsWithStatus);
+      console.log(chainType, "final result:", result.blockNumber, result.result, includesAllOutputsWithStatus ? result.status : "");
     } else {
       const {nodeUrl} = config[chainType];
       const client = getEthClient(nodeUrl);
-      const result = await multiCall(client, config[chainType], "ethMultiCall");
-      console.log(chainType, "final result:", result.blockNumber, result.result);
+      const result = await multiCall(client, config[chainType], "ethMultiCall", aggregateFuncName, includesAllOutputsWithStatus);
+      console.log(chainType, "final result:", result.blockNumber, result.result, includesAllOutputsWithStatus ? result.status : "");
     }
   }
 }
 
-const config = {
-  WAN: {
-    nodeUrl: process.env.testnetWanRpc,
-    multiCallAddr: "0x14095a721Dddb892D6350a777c75396D634A7d97",
-    targetInfo: [
-      {
-        address:"0xaa5a0f7f99fa841f410aafd97e8c435c75c22821"
-        ,funcName:"getStoremanGroupInfo"
-        ,inputTypes:["bytes32"]
-        ,outputTypes:[["bytes", {"components":[{"name":"groupId","type":"bytes32"},{"name":"status","type":"uint8"},{"name":"deposit","type":"uint256"},{"name":"depositWeight","type":"uint256"},{"name":"selectedCount","type":"uint256"},{"name":"memberCount","type":"uint256"},{"name":"whiteCount","type":"uint256"},{"name":"whiteCountAll","type":"uint256"},{"name":"startTime","type":"uint256"},{"name":"endTime","type":"uint256"},{"name":"registerTime","type":"uint256"},{"name":"registerDuration","type":"uint256"},{"name":"memberCountDesign","type":"uint256"},{"name":"threshold","type":"uint256"},{"name":"chain1","type":"uint256"},{"name":"chain2","type":"uint256"},{"name":"curve1","type":"uint256"},{"name":"curve2","type":"uint256"},{"name":"tickedCount","type":"uint256"},{"name":"minStakeIn","type":"uint256"},{"name":"minDelegateIn","type":"uint256"},{"name":"minPartIn","type":"uint256"},{"name":"crossIncoming","type":"uint256"},{"name":"gpk1","type":"bytes"},{"name":"gpk2","type":"bytes"},{"name":"delegateFee","type":"uint256"}],"name":"info","type":"tuple"}]]
-        ,outputNames:["storemanGroupInfo"]
-        ,args:["0x000000000000000000000000000000000000000000000000006465765f303833"]
-      }
-      ,{
-        address:"0x14095a721Dddb892D6350a777c75396D634A7d97"
-        ,funcName:"getEthBalance"
-        ,inputTypes:["address"]
-        ,outputTypes:["uint256"]
-        ,outputNames:["balance"]
-        ,args:["0x14095a721Dddb892D6350a777c75396D634A7d97"]
-      }
-      ,{
-        address:"0x62dE27e16f6f31d9Aa5B02F4599Fc6E21B339e79"
-        ,funcName:"getFee"
-        ,inputTypes:[{"components":[{"name":"srcChainID","type":"uint256"},{"name":"destChainID","type":"uint256"}],"name":"param","type":"tuple"}]
-        ,outputTypes:[["bytes", {"components":[{"name":"lockFee","type":"uint256"},{"name":"revokeFee","type":"uint256"}],"name":"fee","type":"tuple"}]]
-        ,outputNames:["feeDict"]
-        ,args:[["0x8057414e","0x8000003c"]]
-      }
-    ]
-  },
-  TRX: {
-    nodeUrl: process.env.testnetTrxRpc,
-    // multiCallAddr: "TSDi2voShhfnG1SsuFDGQ2G7cRZPqreaae", // 0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b
-    multiCallAddr: "0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b", // 0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b
-    targetInfo: [
-      {
-        address:"0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b"
-        ,funcName:"getEthBalance"
-        ,inputTypes:["address"]
-        ,outputTypes:["uint256"]
-        ,outputNames:["balance"]
-        ,args:["0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b"] // TSDi2voShhfnG1SsuFDGQ2G7cRZPqreaae
-      }
-      ,{
-        address:"0xb2401225f5ec9b4c1014a93edb3cda19afdd9c1b" // TSDi2voShhfnG1SsuFDGQ2G7cRZPqreaae
-        ,funcName:"getEthBalance"
-        ,inputTypes:["address"]
-        ,outputTypes:["uint256"]
-        ,outputNames:["balance"]
-        ,args:["0x242aa579f130bf6fea5eac12aa6b846fb8b293ab"] // TDGSR64oU4QDpViKfdwawSiqwyqpUB6JUD
-      }
-      ,{
-        address:"TY1JDH5SJzKSmZvDMVX5ozWWyddqmqKWD9"
-        ,funcName:"getFee"
-        ,inputTypes:[{"components":[{"name":"srcChainID","type":"uint256"},{"name":"destChainID","type":"uint256"}],"name":"param","type":"tuple"}]
-        ,outputTypes:[[{"components":[{"name":"lockFee","type":"uint256"},{"name":"revokeFee","type":"uint256"}],"name":"fee","type":"tuple"},"bytes"]]
-        ,outputNames:["feeDict"]
-        ,args:[["0x800000c3","0x8000003c"]]
-      }
-    ]
+function getConfig(includesAllOutputsWithStatus) {
+  const config = {
+    WAN: {
+      nodeUrl: process.env.testnetWanRpc,
+      multiCallAddr: "0xd74fe1137461ea0afa66d9024d7add7286e7ed0e",
+      targetInfo: [
+        {
+          address:"0xaa5a0f7f99fa841f410aafd97e8c435c75c22821"
+          ,funcName:["getStoremanGroupInfo", "getStoremanGroupConfig"]
+          ,inputTypes:["bytes32"]
+          ,outputTypes:[["bytes", {"components":[{"name":"groupId","type":"bytes32"},{"name":"status","type":"uint8"},{"name":"deposit","type":"uint256"},{"name":"depositWeight","type":"uint256"},{"name":"selectedCount","type":"uint256"},{"name":"memberCount","type":"uint256"},{"name":"whiteCount","type":"uint256"},{"name":"whiteCountAll","type":"uint256"},{"name":"startTime","type":"uint256"},{"name":"endTime","type":"uint256"},{"name":"registerTime","type":"uint256"},{"name":"registerDuration","type":"uint256"},{"name":"memberCountDesign","type":"uint256"},{"name":"threshold","type":"uint256"},{"name":"chain1","type":"uint256"},{"name":"chain2","type":"uint256"},{"name":"curve1","type":"uint256"},{"name":"curve2","type":"uint256"},{"name":"tickedCount","type":"uint256"},{"name":"minStakeIn","type":"uint256"},{"name":"minDelegateIn","type":"uint256"},{"name":"minPartIn","type":"uint256"},{"name":"crossIncoming","type":"uint256"},{"name":"gpk1","type":"bytes"},{"name":"gpk2","type":"bytes"},{"name":"delegateFee","type":"uint256"}],"name":"info","type":"tuple"}]]
+          ,outputNames:["storemanGroupInfo"]
+          ,args:["0x000000000000000000000000000000000000000000000000006465765f303833"]
+        }
+        ,{
+          address:"0xd74fe1137461ea0afa66d9024d7add7286e7ed0e"
+          ,funcName:includesAllOutputsWithStatus?["getTokenBalance","getBalance"]:["getEthBalance1","getEthBalance","getBalance"]
+          ,inputTypes:["address"]
+          ,outputTypes:["uint256"]
+          ,outputNames:["balance"]
+          ,args:["0xd74fe1137461ea0afa66d9024d7add7286e7ed0e"]
+        }
+        ,{
+          address:"0x62dE27e16f6f31d9Aa5B02F4599Fc6E21B339e79"
+          ,funcName:"getFee"
+          ,inputTypes:[{"components":[{"name":"srcChainID","type":"uint256"},{"name":"destChainID","type":"uint256"}],"name":"param","type":"tuple"}]
+          ,outputTypes:[{"components":[{"name":"lockFee","type":"uint256"},{"name":"revokeFee","type":"uint256"}],"name":"fee","type":"tuple"}]
+          ,outputNames:["feeDict"]
+          ,args:[["0x8057414e","0x8000003c"]]
+        }
+      ]
+    },
+    TRX: {
+      nodeUrl: process.env.testnetTrxRpc,
+      multiCallAddr: "TDUDJcZiLZRCPSpWXHP6KKqZ2XL3koriw4", // 0x2664B161FA7F6C16AD56A5BD3D1B3231B7166811
+      targetInfo: [
+        {
+          address:"0x2664B161FA7F6C16AD56A5BD3D1B3231B7166811"
+          ,funcName:includesAllOutputsWithStatus?["getTokenBalance","getBalance"]:["getEthBalance1","getEthBalance","getBalance"]
+          ,inputTypes:["address"]
+          ,outputTypes:["uint256"]
+          ,outputNames:["balance"]
+          ,args:["0x2664B161FA7F6C16AD56A5BD3D1B3231B7166811"] // TDUDJcZiLZRCPSpWXHP6KKqZ2XL3koriw4
+        }
+        ,{
+          address:"0x2664B161FA7F6C16AD56A5BD3D1B3231B7166811" // TDUDJcZiLZRCPSpWXHP6KKqZ2XL3koriw4
+          ,funcName:["getEthBalance", "getBalance"]
+          ,inputTypes:["address"]
+          ,outputTypes:["uint256"]
+          ,outputNames:["balance"]
+          ,args:["0x242aa579f130bf6fea5eac12aa6b846fb8b293ab"] // TDGSR64oU4QDpViKfdwawSiqwyqpUB6JUD
+        }
+        ,{
+          address:"TY1JDH5SJzKSmZvDMVX5ozWWyddqmqKWD9"
+          ,funcName:"getFee"
+          ,inputTypes:[{"components":[{"name":"srcChainID","type":"uint256"},{"name":"destChainID","type":"uint256"}],"name":"param","type":"tuple"}]
+          ,outputTypes:[[{"components":[{"name":"lockFee","type":"uint256"},{"name":"revokeFee","type":"uint256"}],"name":"fee","type":"tuple"},"bytes"]]
+          ,outputNames:["feeDict"]
+          ,args:[["0x800000c3","0x8000003c"]]
+        }
+      ]
+    }
   }
+  const chainTypeDict = Object.keys(config).reduce((reduced, next) => {
+    reduced[next] = next;
+    return reduced;
+  }, {});
+  return {config, chainTypeDict};
 }
 
-const chainTypeDict = Object.keys(config).reduce((reduced, next) => {
-  reduced[next] = next;
-  return reduced;
-}, {})
+// var abi = [{"inputs":[{"components":[{"internalType":"address","name":"target","type":"address"},{"internalType":"bytes","name":"callData","type":"bytes"}],"internalType":"structMulticallV2.Call[]","name":"calls","type":"tuple[]"}],"name":"aggregate","outputs":[{"internalType":"uint256","name":"blockNumber","type":"uint256"},{"internalType":"bytes[]","name":"returnData","type":"bytes[]"}],"stateMutability":"view","type":"function"}];
+// console.log("before", MultiCall.aggregateFuncNameDict);
+// MultiCall.setAbi(abi)
+// console.log("after", MultiCall.aggregateFuncNameDict);
 
-test(config, chainTypeDict)
+const {config: configWithoutStatus, chainTypeDict:chainTypeDictWithoutStatus} = getConfig(false);
+const {config, chainTypeDict} = getConfig(true);
+
+test(configWithoutStatus, chainTypeDictWithoutStatus, MultiCall.aggregateFuncNameDict.aggregateWithAll)
+test(config, chainTypeDict, MultiCall.aggregateFuncNameDict.aggregateWithAll, true)
